dff2030437499fb035aa16d6b22064d24d46b57a
Bo Kim <bohyung0110@gmail.com>
2022-02-25 14:53:03 -0800

    added Jeddy solution

--- profile.js
+++ profile.js
@@ -10,3 +10,22 @@ faveColor.addEventListener('click',colorAlert);
 favePlace.addEventListener('click',placeAlert);
 faveRitual.addEventListener('click',ritalAlert);
 
+const profileBtns = document.querySelector('button');
+
+// profileBtns.forEach(btn) => {
+//     btn.addEventListener('click', (event) => {
+//         let favorite = '';
+//         switch (event.target.id) {
+//             case 'color':
+//                 favorite = 'Yellow';
+//                 break;
+//             case 'place':
+//                 favorite = 'My home';
+//                 break;
+//             case 'ritual':
+//                 favorite = 'Sleeping';
+//                 break;
+//         }
+//         alert(`My favorite ${event.target.id} is ${favorite}`);
+//     })
+// })
